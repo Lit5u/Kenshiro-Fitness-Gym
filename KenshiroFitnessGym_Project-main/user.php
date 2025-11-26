0ce11c925b968a99cb9f8ae696c325d652fbbdb2
Lit5u <2023-1-02330@lpu.edu.ph>
2025-11-26 23:23:17 +0800

    Add files via upload

---
+++ KenshiroFitnessGym_Project-main/user.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kenshiro User</title>
+    <link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+    <header>
+      <h1>Kenshiro Fitness Gym</h1>
+      <nav>
+        <ul>
+          <a href="index.html">HOME</a>
+          <a href="login.html">LOGIN</a>
+          <a href="register.html">REGISTER</a>
+           <button id="theme-toggle">
+             <span id="theme-icon">🌙</span>
+          </button>
+        </ul>
+      </nav>
+    </header>
+    <footer>
+      <p>&copy; 2019 Kenshiro Fitness Gym. All Rights Reserved</p>
+    </footer>
+
+    <script src="script.js"></script>
+</body>
+</html>
